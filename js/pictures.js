dea21f9bc81cc9b930c5cf6fe96b1658c77808df
Natasha <natalka.astashova@gmail.com>
2018-06-16 17:20:14 -0500

    Исправила все замечания Тревиса

--- js/pictures.js
+++ js/pictures.js
@@ -1,60 +1,62 @@
+'use strict';
 var COMMENTS_FOR_FOTO = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 var DESCRIPTION = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...', 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......', 'Вот это тачка!'];
 var MIN_LIKES_COUNT = 15;
 var MAX_LIKES_COUNT = 200;
 
-var createArray = function() {
+var createArray = function () {
   var array = [];
-  for (var i = 0; i < 25; i++){
+  for (var i = 0; i < 25; i++) {
     array[i] = {
       url: createUrl(i + 1),
       likes: createLikes(),
       comments: createComments(),
       description: createDescription(DESCRIPTION)
-    }
+    };
   }
   return array;
 };
 
-var createUrl = function(i){
+var createUrl = function (i) {
   var stringUrl = 'photos/' + i + '.jpg';
   return stringUrl;
 };
 
-var createLikes = function() {
+var createLikes = function () {
   var likes = (randomNumber(MAX_LIKES_COUNT - MIN_LIKES_COUNT) + MIN_LIKES_COUNT);
   return likes;
 };
 
-var createOneComment = function(array) {
+var createOneComment = function (array) {
   var numberOfStrings = randomNumber(2) + 1;
+  var stringComents;
   if (numberOfStrings === 1) {
-    var stringComents = array[randomNumber(array.length)];
+    stringComents = array[randomNumber(array.length)];
   } else {
-    var stringComents = array[randomNumber(array.length)] + ' ' + array[randomNumber(array.length)];
+    stringComents = array[randomNumber(array.length)] + ' ' + array[randomNumber(array.length)];
   }
   return stringComents;
 };
 
-var createComments = function() {
+var createComments = function () {
   var array = [];
   for (var i = 0; i < randomNumber(6); i++) {
     array[i] = createOneComment(COMMENTS_FOR_FOTO);
-  };
+  }
   return array;
 };
 
-var createDescription = function(array) {
+var createDescription = function (array) {
   var stringDescription = array[randomNumber(array.length)];
   return stringDescription;
 };
 
-var randomNumber = function(i) {
-  number = Math.floor(Math.random() * i);
+var randomNumber = function (i) {
+  var number = Math.floor(Math.random() * i);
   return number;
 };
 
-var showThumbnails = function(picturesArray) {
+var showThumbnails = function (picturesArray) {
   var templatePicture = document.querySelector('#picture').content.querySelector('.picture__link');
   var picturesContainer = document.querySelector('.pictures');
   for (var i = 0; i < picturesArray.length; i++) {
@@ -65,7 +67,7 @@ var showThumbnails = function(picturesArray) {
     var fragment = document.createDocumentFragment();
     fragment.appendChild(pictureElement);
     picturesContainer.appendChild(fragment);
-  };
+  }
 };
 
 var showBigPicture = function (picture, bigPictureElement) {
@@ -80,7 +82,7 @@ var showBigPicturePopUp = function (bigPictureElement) {
   bigPictureElement.classList.remove('hidden');
 };
 
-var createOneCommentElement = function(text) {
+var createOneCommentElement = function (text) {
   var li = document.createElement('li');
   li.classList.add('social__comment');
   li.classList.add('social__comment--text');
@@ -88,7 +90,7 @@ var createOneCommentElement = function(text) {
   socialComnents.appendChild(li);
   var img = document.createElement('img');
   img.classList.add('social__picture');
-  img.src = 'img/avatar-' + ((randomNumber(6))+1) + '.svg';
+  img.src = 'img/avatar-' + ((randomNumber(6)) + 1) + '.svg';
   img.alt = 'Аватар комментатора фотографии';
   img.width = '35';
   img.height = '35';
@@ -98,16 +100,16 @@ var createOneCommentElement = function(text) {
   li.appendChild(span);
 };
 
-var createAllComments = function(comments){
+var createAllComments = function (comments) {
   for (var i = 0; i < comments.length; i++) {
     createOneCommentElement(comments[i]);
-  };
+  }
 };
 
-var removeCountComments = function() {
+var removeCountComments = function () {
   var socialComentCount = document.querySelector('.social__comment-count');
   socialComentCount.classList.add('visually-hidden');
-  var  socialLoadmore = document.querySelector('.social__loadmore');
+  var socialLoadmore = document.querySelector('.social__loadmore');
   socialLoadmore.classList.add('visually-hidden');
 };
 
@@ -121,8 +123,8 @@ var addClickHandlerToShowBigPicture = function (picturesLinks, i, bigPictureElem
 var addClickHandlerToShowBigPopUp = function (picturesArray, bigPictureElement) {
   var picturesLinks = document.querySelectorAll('.picture__link');
   for (var i = 0; i < picturesLinks.length; i++) {
-    addClickHandlerToShowBigPicture (picturesLinks, i, bigPictureElement, picturesArray);
-  };
+    addClickHandlerToShowBigPicture(picturesLinks, i, bigPictureElement, picturesArray);
+  }
 };
 
 var hideBigPicturePopUp = function (bigPictureElement) {
@@ -142,7 +144,7 @@ var addChangeHandlerToShowNewPhotoForm = function () {
   imgUpload.addEventListener('change', function () {
     imgUploadOverlay.classList.remove('hidden');
   });
-}
+};
 
 var closeImgUpLoad = function () {
   var imgUpload = document.querySelector('#upload-file');
@@ -158,7 +160,7 @@ var imgUploadClassListRemove = function (imgUploadPreviw) {
   var effectsArray = ['effects__preview--heat', 'effects__preview--chrome', 'effects__preview--sepia', 'effects__preview--marvin', 'effects__preview--phobos'];
   for (var i = 0; i < effectsArray.length; i++) {
     imgUploadPreviw.classList.remove(effectsArray[i]);
-  };
+  }
 };
 
 var addEffects = function () {
@@ -194,7 +196,7 @@ var addEffects = function () {
   });
 };
 
-var initializePictures = function() {
+var initializePictures = function () {
   var bigPictureElement = document.querySelector('.big-picture');
   var picturesArray = createArray();
   showThumbnails(picturesArray);
