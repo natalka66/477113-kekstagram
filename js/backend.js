54210cca258f224375d7cf68770385ab30c7f7d5
Natasha <natalka.astashova@gmail.com>
2018-07-08 18:14:25 -0500

    Сократила функцию

--- js/backend.js
+++ js/backend.js
@@ -2,18 +2,10 @@
 (function () {
   window.backend = {};
   window.backend.load = function (onLoad, onError) { // прием данных
-    window.load('https://js.dump.academy/kekstagram/data', undefined, 'GET', function (text) {
-      onLoad(text);
-    }, function (error) {
-      onError(error);
-    });
+    window.load('https://js.dump.academy/kekstagram/data', undefined, 'GET', onLoad, onError);
   };
   window.backend.save = function (data, onLoad, onError) { // отправка данных
-    window.load('https://js.dump.academy/kekstagram', data, 'POST', function (text) {
-      onLoad(text);
-    }, function (error) {
-      onError(error);
-    });
+    window.load('https://js.dump.academy/kekstagram', data, 'POST', onLoad, onError);
   };
 })();
 
